d8a53a10d448e91c6e1b59d0e4f7db43e9beb879
Udayraj <udayraj.m@bizamica.com>
2021-08-23 19:49:23 +0530

    Add dynamic links for Article and Author

--- src/app/App.js
+++ src/app/App.js
@@ -18,6 +18,12 @@ function App() {
     <Router>
       <Header />
       <main>
+          <Route path='/articles/:title'>
+            <Article />
+          </Route>
+          <Route path='/authors/:name'>
+            <Author />
+          </Route>
           <Route path="/about">
             <About />
           </Route>
